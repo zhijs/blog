20098dc955e102a15377a00a6ef2153ce6775563
杨志 <yangzhi@xunlei.com>
2018-10-11 14:56:30 +0800

    fix(blog): gitignore

--- 2018-10/src/videoExample/sigaling-server.js
+++ 2018-10/src/videoExample/sigaling-server.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var app = express();
-var io = require('socket.io')(app);
+var http = require("http");
+var server=http.createServer(app);
+var io = require('socket.io').listen(server);
 var fs = require('fs');
 var path = require('path')
 
-let offerCreated = false;
 app.listen(8081, function() {
   console.log('server running on localhost:8081')
 });
 
-pp.use(express.static(path.join(__dirname, 'public')))
-
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(function(req, res) {
+  console.log('path--', req.path)
+  fs.readFile(__dirname + `/${req.path}.html`, function(err, data) {
+    res.writeHead(200)
+    res.end(data);
+  })
+})
 io.on('connection', function (socket) {
   
   // 用于却别是否已经创建offer完毕，另外一方需要采用createAnser 应答
