2ec5b2ae79311deeadb8f28f5f9686d7a5342e75
yangzhi <2560418273@qq.com>
2018-10-11 14:26:45 +0800

    signling

--- 2018-10/src/videoExample/sigaling-server.js
+++ 2018-10/src/videoExample/sigaling-server.js
@@ -15,25 +15,26 @@ function handler (req, res) {
 
 io.on('connection', function (socket) {
   
+  console.log('收到连接')
   // 用于却别是否已经创建offer完毕，另外一方需要采用createAnser 应答
-  io.emit('offerState', {
+  socket.emit('offerState', {
     offerCreated
   })
   // 收到A的描述信息，转发给B
-  io.on('offer', function (data) {
+  socket.on('offer', function (data) {
     offerCreated = true
-    console.log('服务端 收到offer包', data)
-    io.emit('offer', data)
+    console.log('服务端收到offer包', data)
+    socket.emit('offer', data)
   });
 
   // 收到ICE信息
-  io.on('swapcandidate', function(data) {
+  socket.on('swapcandidate', function(data) {
     console.log('服务端 收到ICE包', data)
-    io.emit('swapcandidate', data)
+    socket.emit('swapcandidate', data)
   })
 
-  io.on('answer', function(data) {
-    io.emit('answer', data)
+  socket.on('answer', function(data) {
+    socket.emit('answer', data)
   })
 });
       
