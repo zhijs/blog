46074f53e6428b593714dcfd390beb219e3868a6
杨志 <yangzhi@xunlei.com>
2018-10-11 13:03:22 +0800

    feat(blog): blog
    webrct html

--- 2018-10/src/videoExample/sigaling-server.js
+++ 2018-10/src/videoExample/sigaling-server.js
@@ -2,6 +2,7 @@ var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
 
+let offerCreated = false;
 app.listen(8081);
 console.log('server running on localhost:8081')
 function handler (req, res) {
@@ -13,16 +14,26 @@ function handler (req, res) {
 }
 
 io.on('connection', function (socket) {
+  
+  // 用于却别是否已经创建offer完毕，另外一方需要采用createAnser 应答
+  io.emit('offerState', {
+    offerCreated
+  })
   // 收到A的描述信息，转发给B
-  socket.on('answerOffer', function (data) {
+  socket.on('offer', function (data) {
+    offerCreated = true
     console.log('服务端 收到offer包', data)
-    io.emit('answerOffer', data)
+    io.emit('offer', data)
   });
 
   // 收到ICE信息
-  socket.on('iceSwop', function(data) {
+  socket.on('swapcandidate', function(data) {
     console.log('服务端 收到ICE包', data)
-    io.emit('iceSwop', data)
+    io.emit('swapcandidate', data)
+  })
+
+  socket('answer', function(data) {
+    io.emit('answer', data)
   })
 });
       
